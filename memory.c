d966f4ae053a279a07874cfbd44c41ff90abd5c7
iiekezie <iiekezie@gmail.com>
2023-11-16 00:03:31 -0800

    Simple Shell

---
+++ memory.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/*
+ * File: memory.c
+ * Auth: Ifeanyi I Ekezie
+ */
+
+/**
+ * bfree - frees a pointer and NULLs the address
+ * @ptr: address of the pointer to free
+ *
+ * Return: 1 if freed, otherwise 0.
+ */
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
